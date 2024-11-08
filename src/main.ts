6b1f384d80d4f5768d1427c1647b8491e0187203
alvaro medrano <aljak1815@gmail.com>
2024-11-08 16:32:10 -0500

    add documentation api SW

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Biblioteca - CPM')
+    .setDescription('The biblioteca API description')
+    .setVersion('2.0')
+    .addTag('Biblioteca')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -14,6 +25,6 @@ async function bootstrap() {
     }),
   );
   app.setGlobalPrefix('api/v2');
-  await app.listen(4000);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
