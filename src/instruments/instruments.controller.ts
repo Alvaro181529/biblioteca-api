9cb9ab0b2b0ce0f0bdc37492ef66c46ad2dfcffa
alvaro medrano <aljak1815@gmail.com>
2024-12-03 04:47:36 -0400

    updated

--- src/instruments/instruments.controller.ts
+++ src/instruments/instruments.controller.ts
@@ -7,20 +7,24 @@ import {
   Param,
   Delete,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { InstrumentsService } from './instruments.service';
 import { CreateInstrumentDto } from './dto/create-instrument.dto';
 import { UpdateInstrumentDto } from './dto/update-instrument.dto';
+import { Roles } from 'src/users/utilities/common/user-role.enum';
+import { AuthorizeGuard } from 'src/users/utilities/guards/authorization.guards';
+import { AuthenticationGuard } from 'src/users/utilities/guards/authentication.guards';
 
 @Controller('instruments')
 export class InstrumentsController {
   constructor(private readonly instrumentsService: InstrumentsService) {}
-
+  @UseGuards(AuthenticationGuard, AuthorizeGuard([Roles.ADMIN, Roles.ROOT]))
   @Post()
   async create(@Body() createInstrumentDto: CreateInstrumentDto) {
     return await this.instrumentsService.create(createInstrumentDto);
   }
-
+  @UseGuards(AuthenticationGuard)
   @Get()
   async findAll(
     @Query('query') query: string = '',
@@ -31,12 +35,12 @@ export class InstrumentsController {
     const pageSizeNumber = parseInt(pageSize, 10);
     return this.instrumentsService.findAll(pageNumber, pageSizeNumber, query);
   }
-
+  @UseGuards(AuthenticationGuard, AuthorizeGuard([Roles.ADMIN, Roles.ROOT]))
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return await this.instrumentsService.findOne(+id);
   }
-
+  @UseGuards(AuthenticationGuard, AuthorizeGuard([Roles.ADMIN, Roles.ROOT]))
   @Patch(':id')
   async update(
     @Param('id') id: string,
@@ -44,7 +48,7 @@ export class InstrumentsController {
   ) {
     return await this.instrumentsService.update(+id, updateInstrumentDto);
   }
-
+  @UseGuards(AuthenticationGuard, AuthorizeGuard([Roles.ADMIN, Roles.ROOT]))
   @Delete(':id')
   async remove(@Param('id') id: string) {
     return await this.instrumentsService.remove(+id);
